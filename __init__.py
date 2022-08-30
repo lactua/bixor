2cc0d9a5b66095162953ffb087293f7a2c9de4cc
Lactua <82546547+lactua@users.noreply.github.com>
2022-08-30 17:17:51 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ def encrypt(
     content_bins = ' '.join([bin(ord(char))[2:] for char in content])
     key_bins = ''.join([bin(ord(char))[2:] for char in key])
 
-    return ''.join([' ' if char == ' ' else '1' if char == key_bins[index - len(key_bins) * (index // len(key_bins))] else '0' for index, char in list(enumerate(content_bins))])
+    return ''.join([' ' if char == ' ' else '0' if char == key_bins[index - len(key_bins) * (index // len(key_bins))] else '1' for index, char in list(enumerate(content_bins))])
 
 def decrypt(
     encrypted_bins: str,
@@ -19,7 +19,7 @@ def decrypt(
 
     key_bins = ''.join([bin(ord(char))[2:] for char in key])
 
-    decrypted_bins = ''.join([' ' if char == ' ' else '1' if char == key_bins[index - len(key_bins) * (index // len(key_bins))] else '0' for index, char in list(enumerate(encrypted_bins))])
+    decrypted_bins = ''.join([' ' if char == ' ' else '0' if char == key_bins[index - len(key_bins) * (index // len(key_bins))] else '1' for index, char in list(enumerate(encrypted_bins))])
     decrypted_bins = decrypted_bins.split()
 
     return ''.join([chr(int(char,2)) for char in decrypted_bins])
